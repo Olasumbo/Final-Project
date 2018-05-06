519274bdeee1f20931b5e5f604456176fcfe2490
Olasumbo <obabalola@evanstonscholars.org>
2018-05-05 19:26:55 -0500

    Minor Change in player

--- src/System.cpp
+++ src/System.cpp
@@ -19,45 +19,48 @@ System::~System()
 char System::choice(Deck * theDeck)
 {
 	bool invalid_choice = false;
-	do
-	{
-		cout << "Hit, Stay, Double, or Exit?" << endl;
-		cout << "Press H to get a card\nPress S to Stay\nPress D to Double\nE for Exit" << endl;
-		char c = ' ';
-		cin >> c;
-		switch(c)
+		do
 		{
-		case 'H':
-		case 'h':
-		{
-			cout << "Hitting!!" << endl;
-			Hit( theDeck ); // This passes the deck into the function
-			break;
-		}
-		case 'S':
-		case 's':
-		{
-			Stay();
-			break;
-		}
-		case 'D':
-		case 'd':
-		{
-			Double(); // if the player wants to
-			break;
-		}
-		case 'E':
-		case 'e':
-		{
-			exit(EXIT_SUCCESS);
-			break;
-		}
-		default:
-			invalid_choice = true;
-			cout << "Invalid Selection. Please try Again." << endl;
-		}
-	}while(invalid_choice);
-	return 0;
+			cout << "Hit, Stay, Double, or Exit?" << endl;
+			cout << "Press H to get a card\nPress S to Stay\nPress D to Double\nE for Exit" << endl;
+			char c = ' ';
+			cin >> c;
+			switch(c)
+			{
+			case 'H':
+			case 'h':
+			{
+				cout << "Hitting!!" << endl;
+				Hit( theDeck ); // This passes the deck into the function
+				invalid_choice = false;
+				break;
+			}
+			case 'S':
+			case 's':
+			{
+				Stay();
+				invalid_choice = false;
+				break;
+			}
+			case 'D':
+			case 'd':
+			{
+				Double(); // if the player wants to
+				invalid_choice = false;
+				break;
+			}
+			case 'E':
+			case 'e':
+			{
+				exit(EXIT_SUCCESS);
+				break;
+			}
+			default:
+				invalid_choice = true;
+				cout << "Invalid Selection. Please try Again." << endl;
+			}
+		}while(invalid_choice);
+		return 0;
 }
 
 void System::initBet()
